e9baeb25d54a48e8cf68497d97e7595de57b7254
Arystan <arystankaliakparov@gmai.com>
2019-11-26 01:42:19 +0600

    images

---
+++ src/images/index.js
@@ -0,0 +1,12 @@
+export default [
+  require('./tour1.jpg'),
+  require('./tour2.jpg'),
+  require('./tour3.jpg'),
+  require('./tour4.jpg'),
+  require('./tour5.jpg'),
+  require('./tour6.jpg'),
+  require('./tour7.jpg'),
+  require('./tour8.jpg'),
+  require('./tour9.jpg'),
+  require('./tour10.jpg'),
+];
